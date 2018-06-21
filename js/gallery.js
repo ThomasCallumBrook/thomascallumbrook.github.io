074e64a1fd600f9f84d17f63be331dd23ac58f89
Thomas Callum Brook <thomascallumbrook@gmail.com>
2018-06-21 13:59:18 +0100

    Preload gallery images in JS

--- js/gallery.js
+++ js/gallery.js
@@ -6,19 +6,24 @@ function activateGallery() {
   let mainImage = document.querySelector("#gallery-photo img");
 
   thumbnails.forEach(function(thumbnail) {
+    // Preload large images.
+    largeVersion = new Image();
+    largeVersion.src = thumbnail.dataset.largeVersion;
+
     thumbnail.addEventListener("click", function() {
-      // Set clicked image as main image.
+      // Set clicked image as display image.
       let newImageSrc = thumbnail.dataset.largeVersion;
       mainImage.setAttribute("src", newImageSrc);
-       // Change which image is current.
-       let c = "current"
-      document.querySelector(`.${c}`).classList.remove(c);
-      thumbnail.parentNode.classList.add(c);
+
+      // Change which image is current.
+      document.querySelector(".current").classList.remove("current");
+      thumbnail.parentNode.classList.add("current");
 
       // Update image info.
       let galleryInfo = document.querySelector("#gallery-info");
       let title       = galleryInfo.querySelector(".title");
       let description = galleryInfo.querySelector(".description");
+
       title.innerHTML       = thumbnail.dataset.title;
       description.innerHTML = thumbnail.dataset.description;
     });
